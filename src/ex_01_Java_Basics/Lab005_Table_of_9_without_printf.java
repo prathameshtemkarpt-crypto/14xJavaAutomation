4ff78ef2a70fcddbb97a08b936edb1afb1b1ad63
prathameshtemkarpt-crypto <prathameshtemkar.pt@gmail.com>
2025-09-14 19:48:37 +0530

    5th Java program for Table of 9 without printf method

---
+++ src/ex_01_Java_Basics/Lab005_Table_of_9_without_printf.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basics;
+
+public class Lab005_Table_of_9_without_printf {
+    public static void main(String[] args) {
+        int table = 9;
+        System.out.println("The table of 9:\n");
+        System.out.println("9 x 1 = "+ table*1);
+        System.out.println("9 x 2 = "+ table*2);
+        System.out.println("9 x 3 = "+ table*3);
+        System.out.println("9 x 4 = "+ table*4);
+        System.out.println("9 x 5 = "+ table*5);
+        System.out.println("9 x 6 = "+ table*6);
+        System.out.println("9 x 7 = "+ table*7);
+        System.out.println("9 x 8 = "+ table*8);
+        System.out.println("9 x 9 = "+ table*9);
+        System.out.println("9 x 10 = "+ table*10);
+    }
+}
