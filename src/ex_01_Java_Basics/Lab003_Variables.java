94963463fa6700f3e43efba5929ae31dcd739281
prathameshtemkarpt-crypto <prathameshtemkar.pt@gmail.com>
2025-09-14 18:06:50 +0530

    3rd Java program for Variables

---
+++ src/ex_01_Java_Basics/Lab003_Variables.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab003_Variables {
+    public static void main(String[] args) {
+        //int a=5;
+        int b=10;
+        int a=65+1;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("The value of B is:" +b);
+    }
+}
