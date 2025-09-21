4558f4c4e5465d694a5a00d24b7571be99222f81
prathameshtemkarpt-crypto <prathameshtemkar.pt@gmail.com>
2025-09-21 19:05:31 +0530

    Numeric datatypes

---
+++ src/ex_01_Java_Basics/Lab007_Constants.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basics;
+
+public class Lab007_Constants {
+    public static void main(String[] args) {
+
+        int a = 10;
+        a = 20;
+
+        System.out.println(+a);
+
+        final int b = 30;
+        b = 40;   //cannot assign a value to final variable b
+
+
+    }
+    }
+
