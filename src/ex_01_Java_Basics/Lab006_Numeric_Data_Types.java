9177cc34f9b5a84edcd5e99ecf4ed3f6d7f7bbd5
prathameshtemkarpt-crypto <prathameshtemkar.pt@gmail.com>
2025-09-21 14:53:04 +0530

    Numeric datatypes

---
+++ src/ex_01_Java_Basics/Lab006_Numeric_Data_Types.java
@@ -0,0 +1,37 @@
+package ex_01_Java_Basics;
+
+public class Lab006_Numeric_Data_Types {
+    public static void main(String[] args) {
+
+        byte b = 10;
+        short s = 20;
+        int i = 30;
+        char c = 'a';
+        char c1 = 'A';
+
+        long phone = 7977495428l;
+        long phone2 = 917977495428L;
+
+        float f = 3.14f;
+        float f2 = 3.14F;
+
+        double d = 3.663353;
+        double d1 = 3.663353d;
+        double d2 = 3.663353d;
+
+        System.out.println("The value of byte: " +b);
+        System.out.println("The value of short: " +s);
+        System.out.println("The value of int: " +i);
+        System.out.println("The value of char: " +c);
+        System.out.println("The value of char1: " +c1);
+        System.out.println("The value of long: " +phone);
+        System.out.println("The value of long2: " +phone2);
+        System.out.println("The value of float: " +f);
+        System.out.println("The value of float2: " +f2);
+        System.out.println("The value of double: " +d);
+        System.out.println("The value of double1: " +d1);
+        System.out.println("The value of double2: " +d2);
+
+
+    }
+}
