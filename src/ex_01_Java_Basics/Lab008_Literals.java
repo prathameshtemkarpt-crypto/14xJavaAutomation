92a70e511d501967de3654f7e057c907e4a012d5
prathameshtemkarpt-crypto <prathameshtemkar.pt@gmail.com>
2025-09-21 20:10:52 +0530

    Literals

---
+++ src/ex_01_Java_Basics/Lab008_Literals.java
@@ -0,0 +1,35 @@
+package ex_01_Java_Basics;
+
+public class Lab008_Literals {
+    public static void main(String[] args) {
+
+        // Integer literals
+        int integerLiteral = 123;
+        long longLiteral = 1234567890L;
+
+        // Floating-point literals
+        float floatLiteral = 3.14f;
+        double doublLiteral = 3.146654d;
+
+        // Character literals
+        char characterLiteral = 'A';
+
+        // String literal
+        String stringLiteral = "Aai Baba";
+
+        // Boolean literals
+        boolean booleanLiteralTrue = true;
+        boolean booleanLiteralFalse = false;
+
+        System.out.println("Integer literals =" +integerLiteral);
+        System.out.println("Long Literal =" +longLiteral);
+        System.out.println("Float Literal =" +floatLiteral);
+        System.out.println("Doubl Literal =" +doublLiteral);
+        System.out.println("Character Literal =" +characterLiteral);
+        System.out.println("String Literal =" +stringLiteral);
+        System.out.println("Boolean Literal True =" +booleanLiteralTrue);
+        System.out.println("Boolean Literal False =" +booleanLiteralFalse);
+
+
+    }
+}
